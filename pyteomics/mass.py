85eed89c1ba08c0d65a36167412aad8239994fb0
Anton Goloborodko <goloborodko.anton@gmail.com>
2011-07-19 10:17:28 +0200

    Massive changes in mass.py: - rename *_amino_acid_* to *_aa_*; - fix Composition.__from_formula; - massive fixes in documentation; - nist_mass['default'] renamed to nist_mass[0];

--- pyteomics/mass.py
+++ pyteomics/mass.py
@@ -1,11 +1,15 @@
 """
-mass - calculate masses of molecules and ions
-=============================================
+mass - molecular masses and isotope distributions
+=================================================
 
 Summary
 -------
 
-
+This module defines general functions for mass and isotope abundance
+calculations. For most of the functions, a user may define a studied
+substance in various formats but all of them would be reduced to the
+:py:func:`Composition <Composition.__init__>` object describing its
+chemical composition.
 
 Classes
 -------
@@ -40,13 +44,13 @@ Data
   :py:data:`nist_mass` - a dict with exact masses of the most abundant
   isotopes.
 
-  :py:data:`std_amino_acid_comp` - a dict with the elemental compositions
+  :py:data:`std_aa_comp` - a dict with the elemental compositions
   of the standard twenty amino acid residues.
 
   :py:data:`std_ion_comp` - a dict with the relative elemental
   compositions of the standard peptide fragment ions.
 
-  :py:data:`std_amino_acid_mass` - a dict with the monoisotopic masses
+  :py:data:`std_aa_mass` - a dict with the monoisotopic masses
   of the standard twenty amino acid residues.
 
 .. ipython::
@@ -62,44 +66,45 @@ Data
 import math
 import numpy
 import parser
+from auxiliary import PyteomicsError
 
 nist_mass = {
     'H': {1: (1.0078250320710, 0.99988570),
           2: (2.01410177784, 0.00011570),
           3: (3.016049277725, 0.0),
-          'default': (1.0078250320710, 0.99988570)},
+          0: (1.0078250320710, 0.99988570)},
     
     'H+': {1: (1.00727646677, 1.0),
-           'default': (1.00727646677, 1.0)},
+           0: (1.00727646677, 1.0)},
 
-    'e*': {'default': (0.00054857990943, 1.0)}, 
+    'e*': {0: (0.00054857990943, 1.0)}, 
 
     'C': {12: (12.0000000, 0.98938),
           13: (13.0033548378, 0.01078),
           14: (14.0032419894, 0.0),
-          'default': (12.0000000, 0.98938)},
+          0: (12.0000000, 0.98938)},
 
     'N': {14: (14.00307400486, 0.9963620),
           15: (15.00010889827, 0.0036420),
-          'default': (14.00307400486, 0.9963620)},
+          0: (14.00307400486, 0.9963620)},
 
     'O': {16: (15.9949146195616, 0.9975716),
           17: (16.9991317012, 0.000381),
           18: (17.99916107, 0.0020514),
-          'default': (15.9949146195616, 0.9975716)},
+          0: (15.9949146195616, 0.9975716)},
     
     'S': {32: (31.9720710015, 0.949926),
           33: (32.9714587615, 0.00752),
           34: (33.9678669012, 0.042524),
           36: (35.9670807620, 0.00011),
-          'default': (31.9720710015, 0.949926)},
+          0: (31.9720710015, 0.949926)},
     }
 """
 A dict with the exact element masses downloaded from the NIST website:
 http://www.nist.gov/pml/data/comp.cfm . For each element, an entry
 contains the masses and relative abundances of several abundant
-isotopes and a separate "default" entry with the data for the most abundant
-isotope.
+isotopes and a separate default entry with a zero key and the data for
+the most abundant isotope.
 
 .. ipython::
    
@@ -115,7 +120,7 @@ def _parse_isotope_string(label):
     the isotope number.
 
     >>> _parse_isotope_string('C')
-    ('C', 'default')
+    ('C', 0)
     >>> _parse_isotope_string('C[12]')
     ('C', 12)    
     """
@@ -123,19 +128,19 @@ def _parse_isotope_string(label):
         isotope_num = int(label[label.find('[')+1:-1])
         element_name = label[:label.find('[')]
     else:
-        isotope_num = 'default'
+        isotope_num = 0
         element_name = label
     return (element_name, isotope_num)
 
-# Initialize std_amino_acid_comp before the Composition class
+# Initialize std_aa_comp before the Composition class
 # description, fill it later.
-std_amino_acid_comp = {}
+std_aa_comp = {}
 """A dictionary with elemental compositions of the twenty standard
 amino acid residues and standard H- and -OH terminal groups.
 
 .. ipython::
    
-    In [2]: pprint(pyteomics.mass.std_amino_acid_comp)
+    In [2]: pprint(pyteomics.mass.std_aa_comp)
 """
 
 class Composition(dict):
@@ -162,18 +167,19 @@ class Composition(dict):
         return result
     
     def _from_parsed_sequence(self, parsed_sequence,
-                              amino_acid_comp=std_amino_acid_comp):
+                              aa_comp=std_aa_comp):
         self.clear()
         for aa in parsed_sequence:
-            for elem, cnt in amino_acid_comp[aa].items():
-                self[elem] = self.get(elem, 0) + amino_acid_comp[aa][elem]
+            for elem, cnt in aa_comp[aa].items():
+                self[elem] = self.get(elem, 0) + aa_comp[aa][elem]
     
-    def _from_sequence(self, sequence, amino_acid_comp=std_amino_acid_comp):
+    def _from_sequence(self, sequence, aa_comp=std_aa_comp):
         self.clear()
         parsed_sequence = parser.parse_sequence(
             sequence,
-            show_standard_term_groups=True)
-        self._from_parsed_sequence(parsed_sequence)
+            labels=aa_comp.keys(),
+            show_unmodified_termini=True)
+        self._from_parsed_sequence(parsed_sequence, aa_comp)
 
     def _from_formula(self, formula, mass_data=nist_mass):
         prev_chem_symbol_start = len(formula)
@@ -186,9 +192,24 @@ class Composition(dict):
                     try:
                         num_atoms = int(formula[i+1:prev_chem_symbol_start])
                     except ValueError:
-                        raise Exception(
-                            'Chemical formula is not parseable: %s' % formula)
-                element_found = False
+                        raise PyteomicsError(
+                            'Badly-formed number of atoms: %s' % formula)
+                    
+                if formula[i] == ']':
+                    bra_pos = formula.rfind('[', 0, i)
+                    if bra_pos == -1:
+                        raise PyteomicsError(
+                            'Badly-formed isotope number: %s' % formula)
+                    try:
+                        isotope_num = int(formula[bra_pos+1:i])
+                    except ValueError:
+                        raise PyteomicsError(
+                            'Badly-formed isotope number: %s' % formula)
+                    i = bra_pos - 1
+                else:
+                    isotope_num = None
+                    
+                element_found = False        
                 for chem_element in mass_data:
                     if formula.endswith(chem_element, 0, i+1):
                         self[chem_element] = (
@@ -197,9 +218,10 @@ class Composition(dict):
                         prev_chem_symbol_start = i + 1
                         element_found = True
                         break
+                    
                 if not element_found:
-                    raise Exception(
-                        'Chemical formula is not parseable: %s' % formula)
+                    raise PyteomicsError(
+                        'Unknown chemical element in the formula: %s' %formula)
             else:
                 i -= 1
                 continue
@@ -225,30 +247,38 @@ class Composition(dict):
             A polypeptide sequence string in modX notation.
         parsed_sequence : list of str, optional
             A polypeptide sequence parsed into a list of amino acids.
-        amino_acid_comp : dict, optional
+        aa_comp : dict, optional
             A dict with the elemental composition of the amino acids (the
-            default value is std_amino_acid_comp).
+            default value is std_aa_comp).
         mass_data : dict, optional
             A dict with the masses of the chemical elements (the default
             value is nist_mass). It is used for formulae parsing only. 
         """
 
         if 'sequence' in kwargs:
-            amino_acid_comp = kwargs.get('amino_acid_comp',
-                                         std_amino_acid_comp)
-            self._from_sequence(kwargs['sequence'], amino_acid_comp)
+            aa_comp = kwargs.get('aa_comp',
+                                 std_aa_comp)
+            self._from_sequence(kwargs['sequence'], aa_comp)
+            
         elif 'parsed_sequence' in kwargs:
-            amino_acid_comp = kwargs.get('amino_acid_comp',
-                                         std_amino_acid_comp)
+            aa_comp = kwargs.get('aa_comp',
+                                 std_aa_comp)
             self._from_parsed_sequence(kwargs['parsed_sequence'],
-                                       amino_acid_comp)
+                                       aa_comp)
+            
         elif 'formula' in kwargs:
             mass_data = kwargs.get('mass_data', nist_mass)
             self._from_formula(kwargs['formula'], mass_data)
+            
+        elif isinstance(args[0], dict):            
+            self.update(args[0])
+            
         else:
-            self.update(dict(*args, **kwargs))
+            raise PyteomicsError('A Composition object must be specified by'
+                                 'a sequence, parsed sequence, formula or '
+                                 'a dict')
     
-std_amino_acid_comp.update({
+std_aa_comp.update({
     'A':   Composition({'H': 5, 'C': 3, 'O': 1, 'N': 1}),
     'C':   Composition({'H': 5, 'C': 3, 'S': 1, 'O': 1, 'N': 1}),
     'D':   Composition({'H': 5, 'C': 4, 'O': 3, 'N': 1}),
@@ -335,9 +365,9 @@ def calculate_mass(**kwargs):
         If not 0 then m/z is calculated: the mass is increased
         by the corresponding number of proton masses and divided
         by z.
-    amino_acid_comp : dict, optional
+    aa_comp : dict, optional
         A dict with the elemental composition of the amino acids (the
-        default value is std_amino_acid_comp).
+        default value is std_aa_comp).
     mass_data : dict, optional
         A dict with the masses of the chemical elements (the default
         value is nist_mass). 
@@ -353,11 +383,23 @@ def calculate_mass(**kwargs):
     mass_data = kwargs.get('mass_data', nist_mass)
     ion_comp = kwargs.get('ion_comp', std_ion_comp)
     # Make a deep copy of `composition` keyword argument.
-    composition = (dict(kwargs['composition']) if 'composition' in kwargs
+    composition = (dict(kwargs['composition'])
+                   if 'composition' in kwargs
                    else Composition(**kwargs))
 
     if 'ion_type' in kwargs:
-        composition += ion_comp(kwargs['ion_type'])
+        composition += ion_comp[kwargs['ion_type']]
+
+    charge = 0
+    if 'H+' in composition:
+        charge = composition['H+']
+    if 'charge' in kwargs:
+        if charge:
+            raise PyteomicsError(
+                'Charge is specified both by the number of protons and '
+                '\'charge\' in kwargs: %s' % (str(kwargs),))
+        charge = kwargs.get('charge')
+        composition['H+'] = charge
 
     # Calculate mass.
     mass = 0.0
@@ -365,7 +407,7 @@ def calculate_mass(**kwargs):
         element_name, isotope_num = _parse_isotope_string(element)
         if kwargs.get('average', False):
             for isotope_num in mass_data[element_name]:
-                if isotope_num != 'default':
+                if isotope_num:
                     mass += (composition[element_name]
                              * mass_data[element_name][isotope_num][0]
                              * mass_data[element_name][isotope_num][1])
@@ -374,9 +416,8 @@ def calculate_mass(**kwargs):
                      * mass_data[element_name][isotope_num][0])
 
     # Calculate m/z if required.
-    charge = kwargs.get('charge', 0)
     if charge:
-        mass = (mass + mass_data['H+']['default'][0] * charge) / charge
+        mass = mass / charge
     return mass
  
 def most_probable_isotopic_composition(**kwargs):
@@ -403,9 +444,9 @@ def most_probable_isotopic_composition(**kwargs):
     elements_with_isotopes : list of str
         A list of elements to be considered in isotopic distribution
         (by default, every element has isotopic distribution).
-    amino_acid_comp : dict, optional
+    aa_comp : dict, optional
         A dict with the elemental composition of the amino acids (the
-        default value is std_amino_acid_comp).
+        default value is std_aa_comp).
     mass_data : dict, optional
         A dict with the masses of the chemical elements (the default
         value is nist_mass). 
@@ -426,7 +467,7 @@ def most_probable_isotopic_composition(**kwargs):
     # Removing isotopes from the composition.
     for element in composition:
         element_name, isotope_num = _parse_isotope_string(element)
-        if isotope_num != 'default':
+        if isotope_num:
             composition[element_name] = (composition.get(element_name, 0)
                                          + composition.pop(element))
 
@@ -438,8 +479,8 @@ def most_probable_isotopic_composition(**kwargs):
         if not elements_with_isotopes or element in elements_with_isotopes:
             # Take the two most abundant isotopes.
             first_iso, second_iso = sorted(
-                [(i[0], i[1][1]) for i in mass_data[element].iteritems()
-                 if i[0] != 'default'])[:2]
+                [(i[0], i[1][1])
+                 for i in mass_data[element].iteritems() if i[0]])[:2]
             
             # Write the number of isotopes of the most abundant type.
             first_iso_str = _make_isotope_string(element, first_iso[0])
@@ -472,8 +513,8 @@ def binomial_dist(k, n, p):
     return binomial_coeff(n, k) * (p ** k) * (1.0 - p) ** (n - k)
 
 def isotopic_composition_abundance(**kwargs):
-    """Calculate the relative abundance of given isotopic composition
-    of a molecule.
+    """Calculate the relative abundance of a given isotopic composition
+    of a molecule. 
     
     Parameters
     ----------
@@ -491,7 +532,8 @@ def isotopic_composition_abundance(**kwargs):
         The relative abundance of a given isotopic composition.
     """
 
-    composition = (dict(kwargs['composition']) if 'composition' in kwargs
+    composition = (dict(kwargs['composition'])
+                   if 'composition' in kwargs
                    else Composition(**kwargs))
 
     isotopic_composition = {}
@@ -500,11 +542,16 @@ def isotopic_composition_abundance(**kwargs):
     # element and rearrange the elements.
     for element in composition:
         element_name, isotope_num = _parse_isotope_string(element)
-        if ((element_name in isotopic_composition) and
-        (isotope_num == 'default' or
-         'default' in isotopic_composition[element_name])):
-            raise Exception('Either fully specify isotopic composition of '
-                            '%s or do not specify it at all.' % element_name)
+
+        # If there is already an entry for this element and either it
+        # contains a default isotope or newly added isotope is default
+        # then raise an exception.
+        if ((element_name in isotopic_composition)
+            and (not isotope_num
+                 or 0 in isotopic_composition[element_name])):
+            raise PyteomicsError(
+                'Please specify the isotopic numbers of all atoms of '
+                '%s or do not specify them at all.' % element_name)
         else:
             if element_name not in isotopic_composition:
                 isotopic_composition[element_name] = {}
@@ -518,14 +565,14 @@ def isotopic_composition_abundance(**kwargs):
         relative_abundance *= factorial_s3(sum(isotope_dict.values()))
         for isotope_num, isotope_content in isotope_dict.items():
             relative_abundance /= factorial_s3(isotope_content)            
-            if isotope_num != 'default':
+            if isotope_num:
                 relative_abundance *= (
                     mass_data[element_name][isotope_num][1]
                     ** isotope_content)
             
     return relative_abundance
 
-std_amino_acid_mass = {
+std_aa_mass = {
     'G': 57.02146,
     'A': 71.03711,
     'S': 87.03203, 
@@ -552,7 +599,7 @@ amino acid residues.
 
 .. ipython::
    
-    In [2]: pprint(pyteomics.mass.std_amino_acid_mass)
+    In [2]: pprint(pyteomics.mass.std_aa_mass)
 """
 
 def fast_mass(sequence, ion_type=None, charge=None, **kwargs):
@@ -575,9 +622,9 @@ def fast_mass(sequence, ion_type=None, charge=None, **kwargs):
     mass_data : dict, optional
         A dict with the masses of the chemical elements (the default
         value is nist_mass). 
-    amino_acid_mass : dict, optional
+    aa_mass : dict, optional
         A dict with the monoisotopic mass of amino acid residues
-        (default is std_amino_acid_mass);
+        (default is std_aa_mass);
     ion_comp : dict, optional
         A dict with the relative elemental compositions of peptide ion
         fragments (default is std_ion_comp).
@@ -587,20 +634,20 @@ def fast_mass(sequence, ion_type=None, charge=None, **kwargs):
     mass : float
         Monoisotopic mass or m/z of a peptide molecule/ion.
     """
-    amino_acid_mass = kwargs.get('amino_acid_mass', std_amino_acid_mass)
-    mass = sum([amino_acid_mass[i] for i in sequence])
+    aa_mass = kwargs.get('aa_mass', std_aa_mass)
+    mass = sum([aa_mass[i] for i in sequence])
 
     mass_data = kwargs.get('mass_data', nist_mass)
-    mass += mass_data['H']['default'][0] * 2.0 + mass_data['O']['default'][0]
+    mass += mass_data['H'][0][0] * 2.0 + mass_data['O'][0][0]
 
     if ion_type:
         ion_comp = kwargs.get('ion_comp', std_ion_comp)
         mass += sum(
-            [mass_data[element]['default'][0] * ion_comp[ion_type][element]
+            [mass_data[element][0][0] * ion_comp[ion_type][element]
              for element in ion_comp[ion_type]])
         
     if charge:
-        mass = (mass + mass_data['H+']['default'][0] * charge) / charge
+        mass = (mass + mass_data['H+'][0][0] * charge) / charge
 
     return mass
  
