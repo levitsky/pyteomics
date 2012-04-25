7c2c1d4ce82a3d1368b4f0c35abfb31b19a19412
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2012-04-26 00:33:52 +0400

    Minor fixes

--- tests/test_mass.py
+++ tests/test_mass.py
@@ -51,8 +51,6 @@ class MassTest(unittest.TestCase):
                          'a': mass.Composition({'A':-1},
                                                mass_data=self.mass_data)}
         self.mods = {'xx': ('A2B', ['X']), 'yy': ('B2C', ['X', 'Y', 'Z'])}
-        self.kwargs = {'aa_comp': self.aa_comp, 'mass_data': self.mass_data}
-        
 
     def test_fast_mass(self):
         for pep in self.random_peptides:
