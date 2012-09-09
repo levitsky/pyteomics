cf3128197b577ff86acbd6c7a7167157eb63c587
Lev Levitsky <lev.levitsky@phystech.edu>
2012-09-10 00:43:08 +0400

    Start mzidentml

---
+++ pyteomics/mzidentml.py
@@ -0,0 +1,66 @@
+"""
+mzidentml - reader for peptide identification data in mzIdentML format
+======================================================================
+
+Summary
+-------
+
+`mzIdentML <http://www.psidev.info/mzidentml>`_  is one of the standards
+developed by the Proteomics Informatics working group of the HUPO Proteomics
+Standard Initiative.
+
+This module provides a minimalistic way to extract information from mzIdentML
+files. The main idea is the same as in :py:mod:`pyteomics.pepxml`: the top-level
+function :pt:func:`iter_psm` allows iterating over entries in
+`<SpectrumIdentificationResult>` tags, i.e. groups of identifications
+for a certain spectrum. Note that each entry can contain more than one PSM
+(peptide-spectrum match). They are accessible with `"search_hits"` key, just
+like in :py:mod:`pyteomics.pepxml`.
+
+Data access
+-----------
+
+  :py:func:`iter_psm` - iterate through peptide-spectrum matches in a pep.XML 
+  file. Data from a single PSM group are converted to a human-readable dict. 
+
+  :py:func:`get_by_id` - get an element by its ID and extract the data from it.
+
+-------------------------------------------------------------------------------
+"""
+
+#   Copyright 2012 Anton Goloborodko, Lev Levitsky
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from lxml import etree
+
+
+def iter_psm(source):
+    """Parse ``source`` and iterate through peptide-spectrum matches.
+
+    Parameters
+    ----------
+    source : str or file
+        A path or an URL to a target pepXML file or the file object itself.
+
+    Returns
+    -------
+    out : iterator
+       An iterator over the dicts with PSM properties.
+    """
+
+    for _, tag in etree.iterparse(source):
+        if tag.tag == '{{{}}}SpectrumIdentificationResult'.format(
+                tag.nsmap.get(None, '')):
+            yield _psm_from_sid(tag)
+            tag.clear()
