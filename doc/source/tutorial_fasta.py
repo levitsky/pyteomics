a6ed6d8775adbd4ed0b4e9d6679759e497b51c91
Anton Goloborodko <golobor@mit.edu>
2012-10-04 01:46:17 -0400

    Add tutorial_fasta

--- doc/source/tutorial_fasta.py
+++ doc/source/tutorial_fasta.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pyteomics import fasta, parser, mass, achrom, electrochem, auxiliary
 
-import shelve
-
 if not os.path.isfile('./yeast.fasta'):
     print 'Download the FASTA file for Saccharomyces cerevisiae...'
     schema_file = urlretrieve(
@@ -15,14 +13,14 @@ if not os.path.isfile('./yeast.fasta'):
         './yeast.fasta')
     print 'Done!'
 
-print 'Cleave the proteins with Lys-C...'
+print 'Cleave the proteins with trypsin...'
 unique_peptides = set()
 for description, sequence in fasta.read_fasta('./yeast.fasta'):
     new_peptides = parser.cleave(sequence, parser.expasy_rules['trypsin'])
     unique_peptides = set.union(unique_peptides, new_peptides)
-peptides = [{'sequence': i} for i in unique_peptides if len(i) <= 100]
+print 'Done, {0} sequences obtained!'.format(len(unique_peptides))
 
-print 'Done, {0} sequences obtained!'.format(len(peptides))
+peptides = [{'sequence': i} for i in unique_peptides]
 
 print 'Parse peptide sequences...'
 for peptide in peptides:
@@ -32,12 +30,13 @@ for peptide in peptides:
     peptide['length'] = len(peptide['parsed_sequence']) - 2 
 print 'Done!'
 
-peptides = [peptide for peptide in peptides if peptide['length'] <= 200]
+peptides = [peptide for peptide in peptides if peptide['length'] <= 100]
 
 print 'Calculate the mass, charge and m/z...'
 for peptide in peptides:
+    peptide['charge'] = int(round(
+        electrochem.charge(peptide['parsed_sequence'], pH=2.0)))
     peptide['mass'] = mass.calculate_mass(peptide['parsed_sequence'])
-    peptide['charge'] = int(round(electrochem.charge(peptide['parsed_sequence'], 2.0)))
     peptide['m/z'] = mass.calculate_mass(peptide['parsed_sequence'], 
         charge=peptide['charge'])
 print 'Done!'
@@ -53,39 +52,63 @@ for peptide in peptides:
 print 'Done!'
 
 plt.figure()
-plt.hist([peptide['m/z'] for peptide in peptides],
+plt.hist([peptide['m/z'] for peptide in peptides], 
+    bins = 2000,
+    range=(0,4000))
 plt.xlabel('m/z, Th')
 plt.ylabel('# of peptides within 2 Th bin')
 plt.show()
 
 plt.figure()
 plt.hist([peptide['charge'] for peptide in peptides],
-    bins = 40,
-    range=(0,20))
+    bins = 20,
+    range=(0,10))
 plt.xlabel('charge, e')
 plt.ylabel('# of peptides')
 plt.show()
 
 x = [peptide['RT_RP'] for peptide in peptides]
 y = [peptide['RT_normal'] for peptide in peptides]
-a, b, r, stderr = auxiliary.linear_regression(x,y)
 heatmap, xbins, ybins = np.histogram2d(x, y, bins=100)
+heatmap[heatmap == 0] = np.nan
+a, b, r, stderr = auxiliary.linear_regression(x,y)
 
 plt.figure()
 plt.imshow(
     heatmap, 
     extent=(xbins[0], xbins[-1], ybins[0], ybins[-1]),
-    interpolation='none')
+    interpolation='none',
+    origin='lower')
 plt.xlabel('RT on RP, min')
 plt.ylabel('RT on normal phase, min')
 plt.title('All tryptic peptides, RT correlation = {0}'.format(r))
+plt.show()
+
+x = [peptide['m/z'] for peptide in peptides]
+y = [peptide['RT_RP'] for peptide in peptides]
+heatmap, xbins, ybins = np.histogram2d(x, y, 
+    bins=[150, 2000],
+    range=[[0, 4000], [0, 150]])
+heatmap[heatmap == 0] = np.nan
+a, b, r, stderr = auxiliary.linear_regression(x,y)
+
+plt.figure()
+plt.imshow(
+    heatmap, 
+    extent=(xbins[0], xbins[-1], ybins[0], ybins[-1]),
+    interpolation='none',
+    aspect='auto',
+    origin='lower')
+plt.xlabel('m/z, Th')
+plt.ylabel('RT on RP, min')
+plt.title('All tryptic peptides, correlation = {0}'.format(r))
 plt.show()
 
 close_mass_peptides = [peptide for peptide in peptides 
                        if 700.0 <= peptide['m/z'] <= 701.0]
 x = [peptide['RT_RP'] for peptide in close_mass_peptides]
 y = [peptide['RT_normal'] for peptide in close_mass_peptides]
-a, b, r, stderr = auxiliary.linear_regression(x,y)
+a, b, r, stderr = auxiliary.linear_regression(x, y)
 
 plt.figure()
 plt.scatter(x, y)
