efdbefb10414e736456c544156c9520c2781ed33
Lev Levitsky <lev.levitsky@phystech.edu>
2013-01-11 14:18:59 +0400

    Update in tests. New class to solve the issue with numpy in containers

--- tests/test_mzid.py
+++ tests/test_mzid.py
@@ -1,12 +1,14 @@
 import unittest
 from pyteomics.mzid import *
 from data import mzid_spectra
+from itertools import product
 
 class MzidTest(unittest.TestCase):
     def testReadPSM(self):
-        with read('test.mzid') as reader:
-            psms = list(reader)[0:4]
-            self.assertEqual(psms, mzid_spectra)
+        for rec, refs in product((True, False), repeat=2):
+            with read('test.mzid', recursive=rec, retrieve_refs=refs) as reader:
+                psms = list(reader)
+                self.assertEqual(psms, mzid_spectra[(rec, refs)])
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_mzml.py
+++ tests/test_mzml.py
@@ -1,21 +1,13 @@
 import unittest
 from pyteomics.mzml import *
-
+from data import mzml_spectra
 import numpy as np
 
 class MzmlTest(unittest.TestCase):
+    maxDiff = None
     def testReadSpectrum(self):
-        mz_array = np.load('test_mzml_mz.npy')
-        intensity_array = np.load('test_mzml_intensity.npy')
-        for spectrum in read('test.mzML'):
-            self.assertTrue(
-                all(abs(spectrum['m/z array'] - mz_array) < 1.0e-4))
-            self.assertTrue(all(np.equal(
-                spectrum['intensity array'], intensity_array)))
-            self.assertTrue(
-                abs(spectrum['highest observed m/z'] - max(mz_array)) < 1e-4)
-            self.assertTrue(
-                abs(spectrum['lowest observed m/z'] - min(mz_array)) < 1e-4)
+        # http://stackoverflow.com/q/14246983/1258041
+        self.assertEqual(mzml_spectra, list(read('test.mzML')))
             
 if __name__ == '__main__':
     unittest.main()
