911f8fcfd12ff3d834adaa8f33cb67a4aabde741
Lev Levitsky <lev.levitsky@phystech.edu>
2013-04-18 20:42:22 +0400

    Fix issue #2, updates in Composition and its tests.

--- tests/test_mass.py
+++ tests/test_mass.py
@@ -30,28 +30,30 @@ class MassTest(unittest.TestCase):
         
         self.mass_H = mass.nist_mass['H'][0][0]
         self.mass_O = mass.nist_mass['O'][0][0]
-        self.test_aa_mass = {'X':1.0, 'Y':2.0, 'Z':3.0}
+        self.test_aa_mass = {'X': 1.0, 'Y': 2.0, 'Z': 3.0}
         self.random_peptides = [
             ''.join([random.choice('XYZ') for i in range(20)])
             for i in range(10)]
 
-        self.aa_comp = {'X':   mass.Composition({'A':1},
+        self.aa_comp = {'X':   mass.Composition({'A': 1},
                                                 mass_data=self.mass_data),
-                        'Y':   mass.Composition({'B':1},
+                        'Y':   mass.Composition({'B': 1},
                                                 mass_data=self.mass_data),
-                        'Z':   mass.Composition({'C':1},
+                        'Z':   mass.Composition({'C': 1},
                                                 mass_data=self.mass_data),
-                        'H-':  mass.Composition({'D':1},
+                        'H-':  mass.Composition({'D': 1},
                                                 mass_data=self.mass_data),
-                        '-OH': mass.Composition({'E':1},
+                        '-OH': mass.Composition({'E': 1},
                                                 mass_data=self.mass_data),
                         }
 
         self.ion_comp = {'M': mass.Composition({},
                                                mass_data=self.mass_data),
-                         'a': mass.Composition({'A':-1},
+                         'a': mass.Composition({'A': -1},
                                                mass_data=self.mass_data)}
-        self.mods = {'xx': ('A2B', ['X']), 'yy': ('B2C', ['X', 'Y', 'Z'])}
+        self.mods = {'a': mass.Composition(A=1),
+                'b': mass.Composition(B=1)}
+        self.d = {atom: 1 for atom in 'ABCDE'}
 
     def test_fast_mass(self):
         for pep in self.random_peptides:
@@ -61,42 +63,66 @@ class MassTest(unittest.TestCase):
                      for aa in self.test_aa_mass])
                 + self.mass_H * 2.0 + self.mass_O )
 
-    def test_Composition(self):
+    def test_Composition_dict(self):
         # Test Composition from a dict.
         self.assertEqual(
-            mass.Composition({atom:1 for atom in 'ABCDE'},
-                             mass_data=self.mass_data),
-            {atom:1 for atom in 'ABCDE'})
+            mass.Composition(self.d, mass_data=self.mass_data), self.d)
 
+    def test_Composition_formula(self):
         # Test Composition from a formula.
-        self.assertEqual(
+        self.assertEqual(self.d,
             mass.Composition(formula='ABCDE',
-                             mass_data={atom:{0:(1.0,1.0)}
-                                        for atom in 'ABCDE'}),
-            {atom:1 for atom in 'ABCDE'})
+                         mass_data={atom: {0: (1.0, 1.0)} for atom in 'ABCDE'}))
 
-        # Test Composition from a sequence.        
-        self.assertEqual(
-            mass.Composition(sequence='XYZ', aa_comp=self.aa_comp),
-            {atom:1 for atom in 'ABCDE'})
+    def test_Composition_seq(self):
+        # Test Composition from a sequence.
+        self.assertEqual(self.d,
+            mass.Composition(sequence='XYZ', aa_comp=self.aa_comp))
 
+    def test_Composition_pseq(self):
         # Test Composition from a parsed sequence.
         self.assertEqual(
-            mass.Composition(parsed_sequence=['X','Y','Z'],
+            mass.Composition(parsed_sequence=['X', 'Y', 'Z'],
                              aa_comp=self.aa_comp),
-            {atom:1 for atom in 'ABC'})
+            {atom: 1 for atom in 'ABC'})
 
+    def test_Composition_sseq(self):
+        # Test Composition from a split sequence.
+        self.assertEqual(
+            mass.Composition(split_sequence=[('X',), ('Y',), ('Z',)],
+                             aa_comp=self.aa_comp),
+            {atom: 1 for atom in 'ABC'})
+
+    def test_Composition_sum(self):
         # Test sum of Composition objects.
         self.assertEqual(
             mass.Composition(sequence='XXY', aa_comp=self.aa_comp)
             + mass.Composition(sequence='YZZ', aa_comp=self.aa_comp),
-            {atom:2 for atom in 'ABCDE'})
+            {atom: 2 for atom in 'ABCDE'})
 
+    def test_Composition_sub(self):
         # Test subtraction of Composition objects
-        self.assertEqual(
-            mass.Composition(formula='', aa_comp=self.aa_comp)
+        self.assertEqual({}
             - mass.Composition(sequence='XYZ', aa_comp=self.aa_comp),
-            {atom:-1 for atom in 'ABCDE'})
+            {atom: -1 for atom in 'ABCDE'})
+
+    def test_Composition_mul(self):
+        # Test multiplication of Composition by integers
+        self.assertEqual(
+                2 * mass.Composition(sequence='XYZ', aa_comp=self.aa_comp),
+                {atom: 2 for atom in 'ABCDE'})
+        self.assertEqual(
+                mass.Composition(sequence='XYZ', aa_comp=self.aa_comp) * 2,
+                {atom: 2 for atom in 'ABCDE'})
+
+    def test_Composition_positional(self):
+        # Test creation from positional args
+        ac = self.aa_comp.copy()
+        ac.update(self.mods)
+        self.assertEqual(mass.Composition('aXbYZ', aa_comp=ac),
+                {'A': 2, 'B': 2, 'C': 1, 'D': 1, 'E': 1})
+        self.assertEqual(mass.Composition('AB2C3', mass_data=self.mass_data),
+                {'A': 1, 'B': 2, 'C': 3})
 
     def test_calculate_mass(self):
         # Calculate mass by a formula.
