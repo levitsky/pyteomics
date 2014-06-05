c12693b0ae53d1a7c939b30ade378a8b052a91ef
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2014-06-05 18:06:59 +0400

    Remove "read_schema" for tandem

--- tests/test_tandem.py
+++ tests/test_tandem.py
@@ -9,9 +9,8 @@ class TandemTest(unittest.TestCase):
     def testReadPSM(self):
         for func in [read, chain,
                 lambda x, **kw: chain.from_iterable([x], **kw)]:
-            for rs in range(2):
-                with func('test.t.xml', read_schema=rs) as r:
-                    self.assertEqual(list(r), tandem_spectra)
+            with func('test.t.xml') as r:
+                self.assertEqual(list(r), tandem_spectra)
 
 if __name__ == '__main__':
     unittest.main()
