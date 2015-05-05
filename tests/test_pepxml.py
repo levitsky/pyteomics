6ac3f96cc734bd58c02b5f1dc83a20a7c7a0dc25
Lev Levitsky <lev.levitsky@phystech.edu>
2015-05-05 00:40:33 -0400

    Update todo and tests

--- tests/test_pepxml.py
+++ tests/test_pepxml.py
@@ -4,13 +4,19 @@ from pyteomics.pepxml import *
 from data import pepxml_spectra
 
 class PepxmlTest(unittest.TestCase):
-    def setUp(self):
-        self.maxDiff = None
-
+    maxDiff = None
+    _kw = {'full_output': False, 'fdr': 1,
+        'key': lambda x: min(
+            sh['search_score'].get('expect', 1)
+            for sh in x['search_hit'])
+        }
     def testReadPSM(self):
         for rs, it in product([True, False], repeat=2):
             for func in [PepXML, read, chain,
-                    lambda x, **kw: chain.from_iterable([x], **kw)]:
+                    lambda x, **kw: chain.from_iterable([x], **kw),
+                    lambda x, **kw: filter(x, **PepxmlTest._kw),
+                    lambda x, **kw: filter.chain(x, **PepxmlTest._kw),
+                    lambda x, **kw: filter.chain.from_iterable([x], **PepxmlTest._kw)]:
                 with func('test.pep.xml', read_schema=rs, iterative=it) as r:
                     self.assertEqual(list(r), pepxml_spectra)
 
--- tests/test_tandem.py
+++ tests/test_tandem.py
@@ -8,7 +8,10 @@ class TandemTest(unittest.TestCase):
 
     def testReadPSM(self):
         for func in [TandemXML, read, chain,
-                lambda x, **kw: chain.from_iterable([x], **kw)]:
+                lambda x, **kw: chain.from_iterable([x], **kw),
+                lambda x, **kw: filter(x, fdr=1, full_output=False),
+                lambda x, **kw: filter.chain(x, fdr=1, full_output=False),
+                lambda x, **kw: filter.chain.from_iterable([x], fdr=1, full_output=False)]:
             for it in range(2):
                 with func('test.t.xml', iterative=it) as r:
                     self.assertEqual(list(r), tandem_spectra)
