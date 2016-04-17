d49761b02637c16196c9a86682b67ca52c6730ed
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2016-04-17 16:54:17 +0300

    Fix the missing import and python 3 compatibility

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -4,8 +4,9 @@ if [ $# -eq 0 ]; then
     find . -name 'test*.py' -exec echo "Executing" {} \; -exec python {} \; -exec python2 {} \;
 else
     for f; do
-        echo "Executing" "$f"
+        echo "Executing python2" "$f"
         python2 "$f"
+        echo "Executing python" "$f"
         python "$f"
     done
 fi
