81b0de5522946a766bd0e0c23f5aa7bce384fdfa
Lev Levitsky <lev.levitsky@phystech.edu>
2016-04-25 16:25:31 +0300

    Make ensure_bytes strict

--- tests/test_mzml.py
+++ tests/test_mzml.py
@@ -12,7 +12,7 @@ class MzmlTest(unittest.TestCase):
     def testReadSpectrum(self):
         for rs, it in product([True, False], repeat=2):
             for func in [MzML, read, chain,
-                    lambda x, **kw: chain.from_iterable([x], **kw), IndexedMzML]:
+                    lambda x, **kw: chain.from_iterable([x], **kw), IndexedMzML, PreIndexedMzML]:
                 with func('test.mzML', read_schema=rs, iterative=it) as r:
                     # http://stackoverflow.com/q/14246983/1258041
                     self.assertEqual(mzml_spectra, list(r))
