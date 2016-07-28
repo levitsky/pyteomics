5a1e44a6a8c7eca2feffaf3880506766e7fcccbf
Lev Levitsky <lev.levitsky@phystech.edu>
2016-07-28 22:50:43 +0300

    Expand runtests to multiple Python versions

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export PYTHONPATH=".."
 if [ $# -eq 0 ]; then
-    find . -name 'test*.py' -exec echo "Executing python" {} \; -exec python {} \; -exec echo "Executing python2" {} \; -exec python2 {} \;
+    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.4 3.5); for v in "${versions[@]}"; do echo "Executing python${v} $0"; eval "python${v}" "$0"; done' {} \;
 else
     for f; do
-        echo "Executing python2" "$f"
-        python2 "$f"
-        echo "Executing python" "$f"
-        python "$f"
+        for v in 2.7 3.4 3.5; do
+            echo "Executing python${v}" "$f"
+            eval "python${v}" "$f"
+        done
     done
 fi
