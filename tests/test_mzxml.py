8528f4ac7e76e898ee0156ba2045c4388b346e62
Joshua Klein <mobiusklein@gmail.com>
2016-08-24 13:45:44 -0400

    Added mzxml unit tests. Everything failed.

---
+++ tests/test_mzxml.py
@@ -0,0 +1,26 @@
+from os import path
+import pyteomics
+pyteomics.__path__ = [path.abspath(path.join(path.dirname(__file__), path.pardir, 'pyteomics'))]
+from itertools import product
+import unittest
+from pyteomics.mzxml import *
+from data import mzxml_spectra
+import numpy as np
+
+class MzXMLTest(unittest.TestCase):
+    maxDiff = None
+    path = 'test.mzXML'
+
+    def testReadSpectrum(self):
+        for rs, it, ui in product([True, False], repeat=3):
+            for func in [MzXML, read, chain,
+                    lambda x, **kw: chain.from_iterable([x], **kw)]:
+                with func(self.path, read_schema=rs, iterative=it, use_index=ui) as r:
+                    # http://stackoverflow.com/q/14246983/1258041
+                    spec = next(r)
+                    for k in mzxml_spectra:
+                        self.assertEqual(mzxml_spectra[k], spec[k], (func, k))
+                    # self.assertEqual(mzxml_spectra, spec)
+
+if __name__ == '__main__':
+    unittest.main()
