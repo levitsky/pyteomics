0773d9e1a04eafe1e7ac4fb1b9be2b0b85559699
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2017-01-16 17:14:36 +0300

    Add python3.6 in runtests, set default retrieve_refs to True, update changelog

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=".."
 if [ $# -eq 0 ]; then
-    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.3 3.4 3.5); for v in "${versions[@]}"; do echo "Executing python${v} $0"; eval "python${v}" "$0"; done' {} \;
+    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.3 3.4 3.5 3.6); for v in "${versions[@]}"; do echo "Executing python${v} $0"; eval "python${v}" "$0"; done' {} \;
 else
     for f; do
-        for v in 2.7 3.3 3.4 3.5; do
+        for v in 2.7 3.3 3.4 3.5 3.6; do
             echo "Executing python${v}" "$f"
             eval "python${v}" "$f"
         done
