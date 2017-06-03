fb38c400bc3e55e843baf2a683e9968ba218af7c
Lev Levitsky <lev.levitsky@phystech.edu>
2017-06-03 18:04:20 +0300

    Add ms1 parser

---
+++ pyteomics/ms1.py
@@ -0,0 +1,117 @@
+from . import auxiliary as aux
+try:
+    import numpy as np
+except ImportError:
+    np = None
+_array_keys = ['m/z array', 'intensity array']
+
+@aux._file_reader()
+def read(source=None, use_header=False, convert_arrays=2, read_charges=True, dtype=None):
+    """Read an MS1 file and return entries iteratively.
+
+    Read the specified MS1 file, **yield** spectra one by one.
+    Each 'spectrum' is a :py:class:`dict` with four keys: 'm/z array',
+    'intensity array', and 'params'. 'm/z array' and
+    'intensity array' store :py:class:`numpy.ndarray`'s of floats,
+    and 'params' stores a :py:class:`dict` of parameters.
+
+    Parameters
+    ----------
+
+    source : str or file or None, optional
+        A file object (or file name) with data in MGF format. Default is
+        :py:const:`None`, which means read standard input.
+
+    use_header : bool, optional
+        Add the info from file header to each dict. Spectrum-specific parameters
+        override those from the header in case of conflict.
+        Default is :py:const:`True`.
+
+    convert_arrays : bool, optional
+        If :py:const:`False`, m/z and intensities will be returned as regular lists.
+        If :py:const:`True` (default), they will be converted to regular :py:class:`numpy.ndarray`'s.
+        Conversion requires :py:mod:`numpy`.
+    
+    dtype : type or str or dict, optional
+        dtype argument to :py:mod:`numpy` array constructor, one for all arrays or one for each key.
+        Keys should be 'm/z array' and/or 'intensity array'.
+
+    Returns
+    -------
+
+    out : FileReader
+    """
+    if convert_arrays and np is None:
+        raise aux.PyteomicsError('numpy is required for array conversion')
+    dtype_dict = dtype if isinstance(dtype, dict) else {k: dtype for k in _array_keys}
+    header = read_header(source)
+    reading_spectrum = False
+    params = {}
+    masses = []
+    intensities = []
+    if use_header: params.update(header)
+    for line in source:
+        sline = line.strip().split(maxsplit=2)
+        if not reading_spectrum:
+            if sline[0] == 'S':
+                reading_spectrum = True
+                params['scan'] = tuple(sline[1:])
+            # otherwise we are not interested; do nothing, just move along
+        else:
+            if not sline:
+                pass
+            elif sline[0] == 'S':
+                out = {'params': params}
+                if convert_arrays:
+                    data = {'m/z array': masses, 'intensity array': intensities}
+                    for key, values in data.items():
+                        out[key] = np.array(values, dtype=dtype_dict.get(key))
+                else:
+                    out['m/z array'] = masses
+                    out['intensity array'] = intensities
+                yield out
+                del out
+                params = dict(header) if use_header else {}
+                masses = []
+                intensities = []
+            else:
+                if sline[0] == 'I': # spectrum-specific parameters!
+                    params[sline[1]] = sline[2]
+                else: # this must be a peak list
+                    try:
+                        masses.append(float(sline[0]))            # this may cause
+                        intensities.append(float(sline[1]))       # exceptions...\
+                    except ValueError:
+                        raise aux.PyteomicsError(
+                             'Error when parsing %s. Line:\n%s' %
+                             (source, line))
+                    except IndexError:
+                        pass
+
+@aux._keepstate
+def read_header(source):
+    """
+    Read the specified MS1 file, get the parameters specified in the header
+    as a :py:class:`dict`.
+
+    Parameters
+    ----------
+
+    source : str or file
+        File name or file object representing an file in MS1 format.
+
+    Returns
+    -------
+
+    header : dict
+    """
+    with aux._file_obj(source, 'r') as source:
+        header = {}
+        for line in source:
+            l = line.split(maxsplit=2)
+            if l[0] != 'H':
+                break
+            key = l[1]
+            val = l[2].strip()
+            header[key] = val
+        return header
