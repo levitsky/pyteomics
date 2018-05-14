770bf48699ad1d1e8fe59d992519cb336e6c5ad0
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2018-05-14 19:24:10 +0300

    Make retrieve_refs true by default in MzIdentML init

--- tests/test_mzid.py
+++ tests/test_mzid.py
@@ -2,25 +2,29 @@ from os import path
 import pyteomics
 pyteomics.__path__ = [path.abspath(path.join(path.dirname(__file__), path.pardir, 'pyteomics'))]
 import unittest
-from pyteomics.mzid import *
+from pyteomics.mzid import MzIdentML, read, chain
 from pyteomics import auxiliary as aux
 from data import mzid_spectra
 from itertools import product
-from io import BytesIO
 
 class MzidTest(unittest.TestCase):
     maxDiff = None
+    path = 'test.mzid'
     def testReadPSM(self):
         for rec, refs, rs, it, ui in product((True, False), repeat=5):
             for func in [MzIdentML, read, chain,
                     lambda x, **kw: chain.from_iterable([x], **kw)]:
-                with func('test.mzid', recursive=rec, retrieve_refs=refs,
+                with func(self.path, recursive=rec, retrieve_refs=refs,
                         read_schema=rs, iterative=it, use_index=ui) as reader:
                     psms = list(reader)
                     self.assertEqual(psms, mzid_spectra[(rec, refs)])
 
+    def test_skip_empty_values(self):
+        with MzIdentML(self.path, skip_empty_cvparam_values=True, recursive=True, retrieve_refs=True) as f:
+            self.assertEqual(list(f), mzid_spectra[(True, True)])
+
     def test_unit_info(self):
-        with MzIdentML('test.mzid') as handle:
+        with MzIdentML(self.path) as handle:
             for protocol in handle.iterfind("SpectrumIdentificationProtocol"):
                 fragment_tolerance = protocol['FragmentTolerance']
                 self.assertEqual(fragment_tolerance['search tolerance minus value'].unit_info, 'dalton')
