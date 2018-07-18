553e451ffeb0267a8c13274012957620438fff21
Lev Levitsky <lev.levitsky@phystech.edu>
2018-07-19 01:02:21 +0300

    Add format-specific fasta parser classes; change flavour to flavor in parse (sorry)

--- pyteomics/fasta.py
+++ pyteomics/fasta.py
@@ -350,27 +350,152 @@ class TwoLayerIndexedFASTA(IndexedFASTA):
                 return super(TwoLayerIndexedFASTA, self).get_entry(header)
 
 
+class FlavoredMixin():
+    """Parser aimed at a specific FASTA flavor.
+    Subclasses should define `parser` and `header_pattern`.
+    The `parse` argument in :py:meth:`__init__` defines whether description is
+    parsed in output.
+    """
+    def __init__(self, parse=True):
+        if not parse:
+            self.parser = None
 
-@aux._file_reader()
-def read(source=None, ignore_comments=False, parser=None):
-    """Read a FASTA file and return entries iteratively.
+
+class UniProtMixin(FlavoredMixin):
+    header_pattern = r'^(\w+)\|([-\w]+)\|(\w+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
+    header_group = 2
+
+    def parser(self, header):
+        db, ID, entry, name, pairs, _ = re.match(self.header_pattern, header).groups()
+        gid, taxon = entry.split('_')
+        info = {'db': db, 'id': ID, 'entry': entry,
+                'name': name, 'gene_id': gid, 'taxon': taxon}
+        info.update(_split_pairs(pairs))
+        _intify(info, ('PE', 'SV'))
+        return info
+
+class UniProt(UniProtMixin, FASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        FASTA.__init__(self, source, **kwargs)
+        UniProtMixin.__init__(self, parse)
+
+
+class IndexedUniProt(UniProtMixin, TwoLayerIndexedFASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        TwoLayerIndexedFASTA.__init__(self, source, **kwargs)
+        UniProtMixin.__init__(self, parse)
+
+class UniRefMixin(FlavoredMixin):
+    header_pattern = r'^(\S+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
+
+    def parser(self, header):
+        assert 'Tax' in header
+        ID, cluster, pairs, _ = re.match(self.header_pattern, header).groups()
+        info = {'id': ID, 'cluster': cluster}
+        info.update(_split_pairs(pairs))
+        gid, taxon = info['RepID'].split('_')
+        type_, acc = ID.split('_')
+        info.update({'taxon': taxon, 'gene_id': gid, 'type': type_, 'accession': acc})
+        _intify(info, ('n',))
+        return info
+
+
+class UniRef(UniRefMixin, FASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        FASTA.__init__(self, source, **kwargs)
+        UniRefMixin.__init__(self, parse)
+
+
+class IndexedUniRef(UniRefMixin, TwoLayerIndexedFASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        TwoLayerIndexedFASTA.__init__(self, source, **kwargs)
+        UniRefMixin.__init__(self, parse)
+
+
+class UniParcMixin(FlavoredMixin):
+    header_pattern = r'(\S+)\s+status=(\w+)\s*$'
+
+    def parser(self, header):
+        ID, status = re.match(self.header_pattern, header).groups()
+        return {'id': ID, 'status': status}
+
+
+class UniParc(UniParcMixin, FASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        FASTA.__init__(self, source, **kwargs)
+        UniParcMixin.__init__(self, parse)
+
+
+class IndexedUniParc(UniParcMixin, TwoLayerIndexedFASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        TwoLayerIndexedFASTA.__init__(self, source, **kwargs)
+        UniParcMixin.__init__(self, parse)
+
+
+class UniMesMixin(FlavoredMixin):
+    header_pattern = r'^(\S+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
+
+    def parser(self, header):
+        assert 'OS=' in header and 'SV=' in header and 'PE=' not in header
+        ID, name, pairs, _ = re.match(self.header_pattern, header).groups()
+        info = {'id': ID, 'name': name}
+        info.update(_split_pairs(pairs))
+        _intify(info, ('SV',))
+        return info
+
+
+class UniMes(UniMesMixin, FASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        FASTA.__init__(self, source, **kwargs)
+        UniMesMixin.__init__(self, parse)
+
+
+class IndexedUniMes(UniMesMixin, TwoLayerIndexedFASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        TwoLayerIndexedFASTA.__init__(self, source, **kwargs)
+        UniMesMixin.__init__(self, parse)
+
+
+class SPDMixin(FlavoredMixin):
+    header_pattern = r'^([^|]+?)\s*\|\s*(([^|]+?)_([^|]+?))\s*\|\s*([^|]+?)\s*$'
+
+    def parser(self, header):
+        assert '=' not in header
+        ID, gene, gid, taxon, d = re.match(self.header_pattern, header).groups()
+        return {'id': ID, 'gene': gene, 'description': d,
+                'taxon': taxon, 'gene_id': gid}
+
+
+class SPD(SPDMixin, FASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        FASTA.__init__(self, source, **kwargs)
+        SPDMixin.__init__(self, parse)
+
+
+class IndexedSPD(SPDMixin, TwoLayerIndexedFASTA):
+    def __init__(self, source, parse=True, **kwargs):
+        TwoLayerIndexedFASTA.__init__(self, source, **kwargs)
+        SPDMixin.__init__(self, parse)
+
+
+def read(source=None, use_index=False, flavor=None, **kwargs):
+    """Parse a FASTA file. This function serves as a dispatcher between
+    different parsers available in this module.
 
     Parameters
     ----------
     source : str or file or None, optional
         A file object (or file name) with a FASTA database. Default is
         :py:const:`None`, which means read standard input.
-    ignore_comments : bool, optional
-        If True then ignore the second and subsequent lines of description.
-        Default is :py:const:`False`.
-    parser : function or None, optional
-        Defines whether the fasta descriptions should be parsed. If it is a
-        function, that function will be given the description string, and
-        the returned value will be yielded together with the sequence.
-        The :py:data:`std_parsers` dict has parsers for several formats.
-        Hint: specify :py:func:`parse` as the parser to apply automatic
-        format recognition.
-        Default is :py:const:`None`, which means return the header "as is".
+    use_index : bool, optional
+        If :py:const:`True`, the created parser object will be an instance of
+        :py:class:`IndexedFASTA`. If :py:const:`False` (default), it will be
+        an instance of :py:class:`FASTA`.
+    flavor : str or None, optional
+        A supported FASTA header format. If specified, a format-specific
+        parser instance is returned.
+
+        .. note:: See :py:data:`std_parsers` for supported flavors.
 
     Returns
     -------
@@ -378,41 +503,13 @@ def read(source=None, ignore_comments=False, parser=None):
         A named 2-tuple with FASTA header (str or dict) and sequence (str).
         Attributes 'description' and 'sequence' are also provided.
     """
-    f = parser or (lambda x: x)
-    accumulated_strings = []
+    try:
+        parser = std_parsers[flavor and flavor.lower()]
+    except KeyError:
+        raise aux.PyteomicsError('No parser for flavor: {}. Supported flavors: {}'.format(
+            flavor, ', '.join(map(str, std_parsers))))
+    return parser[use_index](source, **kwargs)
 
-    # Iterate through '>' after the file is over to retrieve the last entry.
-    for string in itertools.chain(source, '>'):
-        stripped_string = string.strip()
-
-        # Skip empty lines.
-        if not stripped_string:
-            continue
-
-        is_comment = (stripped_string[0] in '>;')
-        if is_comment:
-            # If it is a continuing comment
-            if len(accumulated_strings) == 1:
-                if not ignore_comments:
-                    accumulated_strings[0] += (' '+stripped_string[1:])
-                else:
-                    continue
-
-            elif accumulated_strings:
-                description = accumulated_strings[0]
-                sequence = ''.join(accumulated_strings[1:])
-
-                # Drop the translation stop sign.
-                if sequence.endswith('*'):
-                    sequence = sequence[:-1]
-                yield Protein(f(description), sequence)
-                accumulated_strings = [stripped_string[1:], ]
-            else:
-                # accumulated_strings is empty; we're probably reading
-                # the very first line of the file
-                accumulated_strings.append(stripped_string[1:])
-        else:
-            accumulated_strings.append(stripped_string)
 
 @aux._file_writer()
 def write(entries, output=None):
@@ -681,60 +778,18 @@ def _intify(d, keys):
         if k in d:
             d[k] = int(d[k])
 
-# definitions for custom parsers
-_uniprotkb_header = r'^(\w+)\|([-\w]+)\|(\w+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
-def _parse_uniprotkb(header):
-    db, ID, entry, name, pairs, _ = re.match(_uniprotkb_header, header).groups()
-    gid, taxon = entry.split('_')
-    info = {'db': db, 'id': ID, 'entry': entry,
-            'name': name, 'gene_id': gid, 'taxon': taxon}
-    info.update(_split_pairs(pairs))
-    _intify(info, ('PE', 'SV'))
-    return info
 
-_uniref_header = r'^(\S+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
-def _parse_uniref(header):
-    assert 'Tax' in header
-    ID, cluster, pairs, _ = re.match(_uniref_header, header).groups()
-    info = {'id': ID, 'cluster': cluster}
-    info.update(_split_pairs(pairs))
-    gid, taxon = info['RepID'].split('_')
-    type_, acc = ID.split('_')
-    info.update({'taxon': taxon, 'gene_id': gid, 'type': type_, 'accession': acc})
-    _intify(info, ('n',))
-    return info
-
-_uniparc_header = r'(\S+)\s+status=(\w+)\s*$'
-def _parse_uniparc(header):
-    ID, status = re.match(_uniparc_header, header).groups()
-    return {'id': ID, 'status': status}
-
-_unimes_header = r'^(\S+)\s+([^=]*\S)((\s+\w+=[^=]+(?!\w*=))+)\s*$'
-def _parse_unimes(header):
-    assert 'OS=' in header and 'SV=' in header and 'PE=' not in header
-    ID, name, pairs, _ = re.match(_unimes_header, header).groups()
-    info = {'id': ID, 'name': name}
-    info.update(_split_pairs(pairs))
-    _intify(info, ('SV',))
-    return info
-
-def _parse_spd(header):
-    assert '=' not in header
-    ID, gene, d = map(lambda s: s.strip(), header.split('|'))
-    gid, taxon = gene.split('_')
-    return {'id': ID, 'gene': gene, 'description': d,
-            'taxon': taxon, 'gene_id': gid}
-
-std_parsers = {'uniprotkb': _parse_uniprotkb, 'uniref': _parse_uniref,
-        'uniparc': _parse_uniparc, 'unimes': _parse_unimes, 'spd': _parse_spd}
+std_parsers = {'uniprot': (UniProt, IndexedUniProt), 'uniref': (UniRef, IndexedUniRef),
+        'uniparc': (UniParc, IndexedUniParc), 'unimes': (UniMes, IndexedUniMes),
+        'spd': (SPD, IndexedSPD), None: (FASTA, IndexedFASTA)}
 """A dictionary with parsers for known FASTA header formats. For now, supported
 formats are those described at
 `UniProt help page <http://www.uniprot.org/help/fasta-headers>`_."""
 
-_std_patterns = {'uniprotkb': _uniprotkb_header, 'uniref': _uniref_header,
-        'uniparc': _uniparc_header, 'unimes': _unimes_header}
+_std_mixins = {'uniprot': UniProtMixin, 'uniref': UniRefMixin,
+        'uniparc': UniParcMixin, 'unimes': UniMesMixin, 'spd': SPDMixin}
 
-def parse(header, flavour='auto', parsers=None):
+def parse(header, flavor='auto', parsers=None):
     """Parse the FASTA header and return a nice dictionary.
 
     Parameters
@@ -742,44 +797,47 @@ def parse(header, flavour='auto', parsers=None):
 
     header : str
         FASTA header to parse
-    flavour : str, optional
+    flavor : str, optional
         Short name of the header format (case-insensitive). Valid values are
         :py:const:`'auto'` and keys of the `parsers` dict. Default is
         :py:const:`'auto'`, which means try all formats in turn and return the
         first result that can be obtained without an exception.
     parsers : dict, optional
         A dict where keys are format names (lowercased) and values are functions
-        that take a header string and return the parsed header. Default is
-        :py:const:`None`, which means use the default dictionary
-        :py:data:`std_parsers`.
+        that take a header string and return the parsed header.
 
     Returns
     -------
 
     out : dict
         A dictionary with the info from the header. The format depends on the
-        flavour."""
-
+        flavor.
+    """
+    parser_function = lambda cls: cls().parser
+    flavor = flavor.lower()
     # accept strings with and without leading '>'
     if header and header[0] == '>':
         header = header[1:]
 
     # choose the format
-    known = parsers or std_parsers
-    if flavour.lower() == 'auto':
+    known = parsers or _std_mixins
+
+    if flavor == 'auto':
         for parser in known.values():
             try:
-                return parser(header)
+                return parser_function(parser)(header)
             except Exception:
                 pass
         raise aux.PyteomicsError('Unknown FASTA header format: ' + header)
-    elif flavour.lower() in known:
+    elif flavor in known:
         try:
-            return known[flavour.lower()](header)
+            return parser_function(known[flavor])(header)
         except Exception as e:
             raise aux.PyteomicsError('Could not parse header as "{}". '
                     'The error message was: {}: {}. Header: "{}"'.format(
-                        flavour, type(e).__name__, e.args[0], header))
+                        flavor, type(e).__name__, e.args[0], header))
+    raise aux.PyteomicsError('Unknown flavor: {}'.format(flavor))
+
 
 chain = aux._make_chain(read, 'read')
 decoy_chain = aux._make_chain(decoy_db, 'decoy_db')
