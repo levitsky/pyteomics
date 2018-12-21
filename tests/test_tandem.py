90f2aec646c56d678bb375daf7ef17fc7491c34d
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2018-12-21 19:38:08 +0300

    Tweak tandem test

--- tests/test_tandem.py
+++ tests/test_tandem.py
@@ -2,8 +2,9 @@ from os import path
 import pyteomics
 pyteomics.__path__ = [path.abspath(path.join(path.dirname(__file__), path.pardir, 'pyteomics'))]
 import unittest
-from pyteomics.tandem import *
+from pyteomics import tandem
 from data import tandem_spectra
+
 
 class TandemTest(unittest.TestCase):
     def setUp(self):
@@ -11,17 +12,18 @@ class TandemTest(unittest.TestCase):
         self.path = 'test.t.xml'
 
     def testReadPSM(self):
-        for func in [TandemXML, read, chain,
-                lambda x, **kw: chain.from_iterable([x], **kw),
-                lambda x, **kw: filter(x, fdr=1, full_output=False),
-                lambda x, **kw: filter.chain(x, fdr=1, full_output=False),
-                lambda x, **kw: filter.chain.from_iterable([x], fdr=1, full_output=False)]:
+        for func in [tandem.TandemXML, tandem.read, tandem.chain,
+                lambda x, **kw: tandem.chain.from_iterable([x], **kw),
+                lambda x, **kw: tandem.filter(x, fdr=1, full_output=False),
+                lambda x, **kw: tandem.filter.chain(x, fdr=1, full_output=False),
+                lambda x, **kw: tandem.filter.chain.from_iterable([x], fdr=1, full_output=False)]:
             for it in range(2):
                 with func(self.path, iterative=it) as r:
                     self.assertEqual(list(r), tandem_spectra)
 
     def test_df(self):
-        df = DataFrame(self.path)
+        df = tandem.DataFrame(self.path)
+        self.assertEqual(df.shape, (1, 29))
 
 if __name__ == '__main__':
     unittest.main()
