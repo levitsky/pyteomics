01ce5a8521c44e262bffdf011cc9e32869561f84
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2019-03-20 17:31:13 +0300

    Update pepxml test

--- tests/test_pepxml.py
+++ tests/test_pepxml.py
@@ -8,11 +8,14 @@ from data import pepxml_results
 
 class PepxmlTest(unittest.TestCase):
     maxDiff = None
+    path = 'test.pep.xml'
+
     _kw = {'full_output': False, 'fdr': 1,
         'key': lambda x: min(
             sh['search_score'].get('expect', 1)
             for sh in x['search_hit'])
         }
+
     def testReadPSM(self):
         for rs, it in product([True, False], repeat=2):
             for func in [PepXML, read, chain,
@@ -20,8 +23,15 @@ class PepxmlTest(unittest.TestCase):
                     lambda x, **kw: filter(x, **PepxmlTest._kw),
                     lambda x, **kw: filter.chain(x, **PepxmlTest._kw),
                     lambda x, **kw: filter.chain.from_iterable([x], **PepxmlTest._kw)]:
-                with func('test.pep.xml', read_schema=rs, iterative=it) as r:
+                with func(self.path, read_schema=rs, iterative=it) as r:
                     self.assertEqual(list(r), pepxml_results)
+
+    def test_index(self):
+        with PepXML(self.path) as reader:
+            self.assertEqual(list(reader.index), ['spectrum_query'])
+            specs = [item['spectrum'] for item in reader]
+            self.assertEqual(list(reader.index['spectrum_query']), specs)
+            self.assertEqual(reader[specs[-1]], pepxml_results[-1])
 
 if __name__ == '__main__':
     unittest.main()
