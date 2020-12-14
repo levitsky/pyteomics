319cf52e7b1e967eac375d11d1ead2f17d12c2d0
Lev Levitsky <lev.levitsky@phystech.edu>
2020-12-14 18:45:43 +0300

    Use quotes in index keys in pepxml test data

--- tests/test_pepxml.py
+++ tests/test_pepxml.py
@@ -6,6 +6,7 @@ import unittest
 from pyteomics.pepxml import PepXML, read, chain, filter
 from data import pepxml_results
 
+
 class PepxmlTest(unittest.TestCase):
     maxDiff = None
     path = 'test.pep.xml'
@@ -14,7 +15,7 @@ class PepxmlTest(unittest.TestCase):
         'key': lambda x: min(
             sh['search_score'].get('expect', 1)
             for sh in x['search_hit'])
-        }
+    }
 
     def testReadPSM(self):
         for rs, it in product([True, False], repeat=2):
@@ -32,6 +33,7 @@ class PepxmlTest(unittest.TestCase):
             specs = [item['spectrum'] for item in reader]
             self.assertEqual(list(reader.index['spectrum_query']), specs)
             self.assertEqual(reader[specs[-1]], pepxml_results[-1])
+
 
 if __name__ == '__main__':
     unittest.main()
