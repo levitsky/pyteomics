d699bca8b7e804330332a53413aebac28c801713
Anna Lefarova <annalefarova@gmail.com>
2021-01-12 22:17:08 +0100

    Added test for mztab 2.0

--- tests/test_mztab.py
+++ tests/test_mztab.py
@@ -7,23 +7,39 @@ from pyteomics import mztab
 
 class MzTabTest(unittest.TestCase):
 
-    path = 'test.mztab'
+    path_mztab1 = 'test.mztab'
+    path_mztab2 = 'test_mztab2'
 
     def test_metadata(self):
-        reader = mztab.MzTab(self.path)
-        self.assertEqual(len(reader.metadata), 208)
-        value = reader.metadata['fixed_mod[1]']
-        self.assertEqual(value, 'CHEMMOD:57.0214637236')
+        reader_mztab1 = mztab.MzTab(self.path_mztab1)
+        self.assertEqual(len(reader_mztab1.metadata), 208)
+        value_from_mztab1 = reader_mztab1.metadata['fixed_mod[1]']
+        self.assertEqual(value_from_mztab1, 'CHEMMOD:57.0214637236')
+
+        reader_mztab2 = mztab.MzTab(self.path_mztab2)
+        self.assertEqual(len(reader_mztab2.metadata), 61)
+        value_from_mztab2 = reader_mztab2.metadata['sample_processing[1]']
+        self.assertEqual(value_from_mztab2, 'MSIO, MSIO:0000148, high performance liquid chromatography')
+
 
     def test_iter(self):
-        reader = mztab.MzTab(self.path)
-        tables = list(reader)
+        reader_mztab1 = mztab.MzTab(self.path_mztab1)
+        tables = list(reader_mztab1)
         self.assertEqual(len(tables), 4)
         [self.assertEqual(len(t), 2) for t in tables]
 
+        reader_mztab2 = mztab.MzTab(self.path_mztab2)
+        tables = list(reader_mztab2)
+        self.assertEqual(len(tables), 3)
+        [self.assertEqual(len(t), 2) for t in tables]
+
     def test_getitem(self):
-        reader = mztab.MzTab(self.path)
-        table = reader['psm']
+        reader_mztab1 = mztab.MzTab(self.path_mztab1)
+        table = reader_mztab1['psm']
+        self.assertIsInstance(table, mztab.pd.DataFrame)
+
+        reader_mztab2 = mztab.MzTab(self.path_mztab2)
+        table = reader_mztab2['sme']
         self.assertIsInstance(table, mztab.pd.DataFrame)
 
 if __name__ == '__main__':
