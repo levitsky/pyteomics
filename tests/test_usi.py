7b8b6612cd7023ea59f7003e4a37f563c9ebed83
Lev Levitsky <lev.levitsky@phystech.edu>
2021-04-05 16:24:11 +0300

    Add test for proxi aggregator

--- tests/test_usi.py
+++ tests/test_usi.py
@@ -5,7 +5,8 @@ pyteomics.__path__ = [path.abspath(path.join(path.dirname(__file__), path.pardir
 
 import unittest
 
-from pyteomics.usi import USI, proxi
+from pyteomics.usi import USI, proxi, AGGREGATOR_KEY
+from pyteomics.auxiliary import PyteomicsError
 
 
 class USITest(unittest.TestCase):
@@ -34,7 +35,26 @@ class PROXITest(unittest.TestCase):
         for a, b in zip(response['intensity array'], usi_proxi_data['intensity array']):
             self.assertAlmostEqual(a, b, 3)
 
+    def test_errors(self):
+        usi_str = "mzspec:MSV000085202:210320_SARS_CoV_2_T:scan:131256"
+        with self.assertRaises(TypeError, msg='Unrecognized backend type: NoneType'):
+            proxi(usi_str, backend=None)
+        with self.assertRaises(PyteomicsError, msg='Unknown PROXI backend name: BackendName'):
+            proxi(usi_str, backend='BackendName')
 
+
+class PROXIAggregatorTest(unittest.TestCase):
+    def test_request(self):
+        usi_str = "mzspec:MSV000085202:210320_SARS_CoV_2_T:scan:131256"
+        response = proxi(usi_str, backend=AGGREGATOR_KEY)
+
+        assert set(usi_proxi_data.keys()) <= set(response.keys())
+
+        for a, b in zip(response['m/z array'], usi_proxi_data['m/z array']):
+            self.assertAlmostEqual(a, b, 3)
+
+        for a, b in zip(response['intensity array'], usi_proxi_data['intensity array']):
+            self.assertAlmostEqual(a, b, 3)
 
 
 if __name__ == "__main__":
