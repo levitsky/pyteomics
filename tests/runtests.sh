54e7d4ea605122d236065493105305218b39bbd8
Lev Levitsky <lev.levitsky@phystech.edu>
2021-12-13 19:10:47 +0300

    Add 3.10 to helper test script

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=".."
 if [ $# -eq 0 ]; then
-    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9); for v in "${versions[@]}"; do command -v "python${v}" > /dev/null 2>&1 && { echo "Executing python${v}" "$0"; eval "python${v}" "$0"; }; done' {} \;
+    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10); for v in "${versions[@]}"; do command -v "python${v}" > /dev/null 2>&1 && { echo "Executing python${v}" "$0"; eval "python${v}" "$0"; }; done' {} \;
 else
     for f; do
-        for v in 2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9; do
+        for v in 2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10; do
             command -v "python${v}" >/dev/null 2>&1 && {
                 if [ -f "$f" ]; then
                     fname="$f"
