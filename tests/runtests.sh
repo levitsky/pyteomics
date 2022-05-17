4ddd7ad2dccb401090bca34c183754370a4a62a5
Lev Levitsky <lev.levitsky@phystech.edu>
2022-05-17 18:58:30 +0300

    Start rewriting the test runner using pyenv

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
-export PYTHONPATH=".."
 if [ $# -eq 0 ]; then
-    find . -name 'test_*.py' -exec bash -c 'declare -a versions=(2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10); for v in "${versions[@]}"; do command -v "python${v}" > /dev/null 2>&1 && { echo "Executing python${v}" "$0"; eval "python${v}" "$0"; }; done' {} \;
+    echo "Installing Python versions..."
+    xargs -a python-versions.txt -I{} pyenv install -s {}
+    echo "Creating environments..."
+    xargs -a python-versions.txt -I{} pyenv virtualenv {} tmp-pyteomics-{}
+    echo "Installing dependencies..."
+    while read pv; do
+        eval "$(pyenv root)/versions/tmp-pyteomics-${pv}/bin/pip" install -U pip wheel
+        eval "$(pyenv root)/versions/tmp-pyteomics-${pv}/bin/pip" install -r ../test-requirements.txt
+        eval "$(pyenv root)/versions/tmp-pyteomics-${pv}/bin/pip" install -U ..
+    done < python-versions.txt
+    echo "Running tests..."
+    find . -name 'test_*.py' -exec bash -c 'echo '"$(pyenv root)"'/versions/tmp-pyteomics-${0}/bin/python $0' {} \;
+    echo "Deleting environments..."
+    xargs -a python-versions.txt -I{} pyenv virtualenv-delete tmp-pyteomics-{}
 else
     for f; do
-        for v in 2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10; do
-            command -v "python${v}" >/dev/null 2>&1 && {
-                if [ -f "$f" ]; then
-                    fname="$f"
-                elif [ -f "test_${f}.py" ]; then
-                    fname="test_${f}.py"
-                fi
-                echo "Executing python${v}" "$fname"; eval "python${v}" "$fname"
-            }
-        done
+        while read pv; do
+            echo "Creating environment..."
+            pyenv virtualenv "$pv" "tmp-pyteomics-single-${pv}"
+            echo "Installing dependencies..."
+            eval "$(pyenv root)/versions/tmp-pyteomics-single-${pv}/bin/pip" install -U pip wheel
+            eval "$(pyenv root)/versions/tmp-pyteomics-single-${pv}/bin/pip" install -r ../test-requirements.txt
+            eval "$(pyenv root)/versions/tmp-pyteomics-single-${pv}/bin/pip" install -U ..
+            echo "Running test..."
+            if [ -f "$f" ]; then
+                fname="$f"
+            elif [ -f "test_${f}.py" ]; then
+                fname="test_${f}.py"
+            fi
+            echo "Executing $(pyenv root)/versions/tmp-pyteomics-single-${pv}/bin/python ${fname}"
+            eval "$(pyenv root)/versions/tmp-pyteomics-single-${pv}/bin/python" "${fname}"
+            echo "Deleting environment..."
+            echo pyenv virtualenv-delete "tmp-pyteomics-single-${pv}"
+            eval pyenv virtualenv-delete "tmp-pyteomics-single-${pv}"
+        done < python-versions.txt
     done
 fi
