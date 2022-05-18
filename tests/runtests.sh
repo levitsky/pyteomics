fb6b0c258d7b76dfc2ae76944cd40cdf4931681f
Lev Levitsky <lev.levitsky@phystech.edu>
2022-05-18 16:34:34 +0300

    Improve test script

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,44 +1,79 @@
 #!/bin/bash
 ROOT="$(pyenv root)"
-if [ $# -eq 0 ]; then
+MODE="single"
+NODEPS=0
+PREFIX_ALL="test_pyteomics_"
+PREFIX_SINGLE="test_pyteomics_single_"
+CLEAN=0
+for i in "$@" ; do
+    if [[ "$i" == "--all" ]] ; then
+        MODE="all"
+    fi
+    if [[ "$i" == "--clean" ]] ; then
+        CLEAN=1
+    fi
+    if [[ "$i" == "--no-deps" ]] ; then
+        NODEPS=1
+    fi
+done
+
+if [[ $MODE == "all" ]] ; then
     echo "Installing Python versions..."
     xargs -a python-versions.txt -I{} pyenv install -s {}
     echo "Creating environments..."
-    xargs -a python-versions.txt -I{} pyenv virtualenv {} tmp-pyteomics-{}
-    echo "Installing dependencies..."
     while read pv; do
-        eval "${ROOT}/versions/tmp-pyteomics-${pv}/bin/pip" install -U pip wheel
-        eval "${ROOT}/versions/tmp-pyteomics-${pv}/bin/pip" install -r ../test-requirements.txt
-        eval "${ROOT}/versions/tmp-pyteomics-${pv}/bin/pip" install -U ..
+        if [[ $CLEAN == 1 ]] || [ ! -d "${ROOT}/versions/${PREFIX_ALL}${pv}" ]; then
+            pyenv virtualenv -f "$pv" "${PREFIX_ALL}${pv}"
+        fi
     done < python-versions.txt
+    if [[ $NODEPS == 0 ]] ; then
+        echo "Installing dependencies..."
+        while read pv; do
+            eval "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/pip" install -U pip wheel
+            eval "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/pip" install -r ../test-requirements.txt
+            eval "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/pip" install -U ..
+        done < python-versions.txt
+    fi
     echo "Running tests..."
     find . -name 'test_*.py' -print | while read f; do
         while read pv; do
-            echo "${ROOT}/versions/tmp-pyteomics-${pv}/bin/python" "$f"
+            echo "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/python" "$f"
+            eval "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/python" "$f"
         done < python-versions.txt
     done
-    echo "Deleting environments..."
-    xargs -a python-versions.txt -I{} pyenv virtualenv-delete -f tmp-pyteomics-{}
+    if [[ $CLEAN == 1 ]] ; then
+        echo "Deleting environments..."
+        xargs -a python-versions.txt -I{} echo pyenv virtualenv-delete -f "${PREFIX_ALL}"{}
+    fi
 else
     for f; do
+        if [ -f "$f" ]; then
+            fname="$f"
+        elif [ -f "test_${f}.py" ]; then
+            fname="test_${f}.py"
+        else
+            continue
+        fi
         while read pv; do
-            echo "Creating environment..."
-            pyenv virtualenv "$pv" "tmp-pyteomics-single-${pv}"
-            echo "Installing dependencies..."
-            eval "${ROOT}/versions/tmp-pyteomics-single-${pv}/bin/pip" install -U pip wheel
-            eval "${ROOT}/versions/tmp-pyteomics-single-${pv}/bin/pip" install -r ../test-requirements.txt
-            eval "${ROOT}/versions/tmp-pyteomics-single-${pv}/bin/pip" install -U ..
-            echo "Running test..."
-            if [ -f "$f" ]; then
-                fname="$f"
-            elif [ -f "test_${f}.py" ]; then
-                fname="test_${f}.py"
+            if [[ $CLEAN == 1 ]] || [ ! -d "${ROOT}/versions/${PREFIX_ALL}${pv}" ]; then
+                echo "Creating environment..."
+                pyenv virtualenv -f "$pv" "${PREFIX_SINGLE}${pv}"
             fi
-            echo "Executing ${ROOT}/versions/tmp-pyteomics-single-${pv}/bin/python ${fname}"
-            eval "${ROOT}/versions/tmp-pyteomics-single-${pv}/bin/python" "${fname}"
-            echo "Deleting environment..."
-            echo pyenv virtualenv-delete -f "tmp-pyteomics-single-${pv}"
-            eval pyenv virtualenv-delete -f "tmp-pyteomics-single-${pv}"
+
+            if [[ $NODEPS == 0 ]] ; then
+                echo "Installing dependencies..."
+                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U pip wheel
+                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -r ../test-requirements.txt
+                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U ..
+            fi
+            echo "Running test..."
+            echo "Executing ${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python ${fname}"
+            eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python" "${fname}"
+            if [[ $CLEAN == 1 ]] ; then
+                echo "Deleting environment..."
+                echo pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
+                eval pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
+            fi
         done < python-versions.txt
     done
 fi
