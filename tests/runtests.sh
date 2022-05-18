224097676e6cf58e2797d7950fbf7b04fade5ec5
Lev Levitsky <lev.levitsky@phystech.edu>
2022-05-18 19:13:12 +0300

    Add --no-run, try to fix counting for --all

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -10,6 +10,8 @@ declare -A exitcodes
 for i in "$@" ; do
     if [[ "$i" == "--all" ]] ; then
         MODE="all"
+    elif [[ "$i" == "--no-run" ]] ; then
+        MODE="no-op"
     fi
     if [[ "$i" == "--clean" ]] ; then
         CLEAN=1
@@ -17,6 +19,7 @@ for i in "$@" ; do
     if [[ "$i" == "--no-deps" ]] ; then
         NODEPS=1
     fi
+
 done
 
 if [[ $MODE == "all" ]] ; then
@@ -43,61 +46,67 @@ if [[ $MODE == "all" ]] ; then
         done < python-versions.txt
     fi
     echo "Running tests..."
-    find . -name 'test_*.py' -print | while read f; do
+    while read f; do
         while read pv; do
             echo "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/python" "$f"
             eval "${ROOT}/versions/${PREFIX_ALL}${pv}/bin/python" "$f"
             exitcodes["$f + py${pv}"]=$?
         done < python-versions.txt
-    done
+    done < <(find . -name 'test_*.py' -print)
 else
-    for f; do
-        if [ -f "$f" ]; then
-            fname="$f"
-        elif [ -f "test_${f}.py" ]; then
-            fname="test_${f}.py"
-        else
-            continue
+    while read pv; do
+        if [[ $CLEAN == 1 ]] ; then
+            echo "Deleting old ${PREFIX_SINGLE}${pv} environment..."
+            echo pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
+            eval pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
         fi
-        while read pv; do
-            if [[ $CLEAN == 1 ]] ; then
-                echo "Deleting old ${PREFIX_SINGLE}${pv} environment..."
-                echo pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
-                eval pyenv virtualenv-delete -f "${PREFIX_SINGLE}${pv}"
-            fi
 
-            if [[ $CLEAN == 1 ]] || [ ! -d "${ROOT}/versions/${PREFIX_SINGLE}${pv}" ]; then
-                echo "Creating environment..."
-                echo pyenv virtualenv -f "$pv" "${PREFIX_SINGLE}${pv}"
-                pyenv virtualenv -f "$pv" "${PREFIX_SINGLE}${pv}"
-            fi
+        if [[ $CLEAN == 1 ]] || [ ! -d "${ROOT}/versions/${PREFIX_SINGLE}${pv}" ]; then
+            echo "Creating environment..."
+            echo pyenv virtualenv -f "$pv" "${PREFIX_SINGLE}${pv}"
+            pyenv virtualenv -f "$pv" "${PREFIX_SINGLE}${pv}"
+        fi
 
-            if [[ $NODEPS == 0 ]] ; then
-                echo "Installing dependencies..."
-                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U pip wheel
-                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U -r ../test-requirements.txt
-                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U ..
+        if [[ $NODEPS == 0 ]] ; then
+            echo "Installing dependencies..."
+            eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U pip wheel
+            eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U -r ../test-requirements.txt
+            eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/pip" install -U ..
+        fi
+    done < python-versions.txt
+
+    if [[ "$MODE" == "single" ]] ; then
+        for f; do
+            if [ -f "$f" ]; then
+                fname="$f"
+            elif [ -f "test_${f}.py" ]; then
+                fname="test_${f}.py"
+            else
+                continue
             fi
-            echo "Running test..."
-            echo "Executing ${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python ${fname}"
-            eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python" "${fname}"
-            exitcodes["$f + py${pv}"]=$?
-        done < python-versions.txt
-    done
+            while read pv; do
+                echo "Executing ${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python ${fname}"
+                eval "${ROOT}/versions/${PREFIX_SINGLE}${pv}/bin/python" "${fname}"
+                exitcodes["$f + py${pv}"]=$?
+            done < python-versions.txt
+        done
+    fi
 fi
 
-total=0
-errors=0
-for key in "${!exitcodes[@]}"; do
-    (( total++ ))
-    if [[ "${exitcodes[$key]}" != 0 ]] ; then
-        (( errors++ ))
-        echo "ERROR: $key => exit code ${exitcodes[$key]}"
-    fi
-done
+if ! [[ "$MODE" == "no-op" ]] ; then
+    total=0
+    errors=0
+    for key in "${!exitcodes[@]}"; do
+        (( total++ ))
+        if [[ "${exitcodes[$key]}" != 0 ]] ; then
+            (( errors++ ))
+            echo "ERROR: $key => exit code ${exitcodes[$key]}"
+        fi
+    done
 
-if [[ $errors == 0 ]] ; then
-    echo "All ${total} test runs complete, there were no errors."
-else
-    echo "All ${total} test runs complete, there were ${errors} errors (see above)."
+    if [[ $errors == 0 ]] ; then
+        echo "All ${total} test runs complete, there were no errors."
+    else
+        echo "All ${total} test runs complete, there were ${errors} errors (see above)."
+    fi
 fi
